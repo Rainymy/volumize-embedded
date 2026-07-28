9c0dcd85e4585c5a19e01f5f86bfbc41912c9ce0
Rainymy <48211766+Rainymy@users.noreply.github.com>
2026-07-28 15:01:34 +0200

    Update debug.rs

--- src/debug.rs
+++ src/debug.rs
@@ -3,6 +3,13 @@ use arduino_hal as hal;
 use hal::port::mode::Output;
 use hal::port::{Pin, PinOps};
 
+#[macro_export]
+macro_rules! log {
+    ($serial:expr, $($arg:tt)*) => {{
+        let _ = ufmt::uwrite!($serial, $($arg)*);
+    }};
+}
+
 /// Blinks the given output pin forever at the given delay (ms).
 /// Call this when something has failed and you want a visible, distinct signal.
 pub fn blink_forever<P: PinOps>(mut led: Pin<Output, P>, delay_ms: u16, blink_count: u16) -> ! {
