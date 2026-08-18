58d21d2efbc33d0644a1dca4da5bfbdedd4ebb77
Rainymy <48211766+Rainymy@users.noreply.github.com>
2026-08-18 04:27:25 +0200

    feat: Setup USB driver with custom info

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use esp_hal::{
     gpio::Pin,
     i2c::master::{Config as I2cConfig, I2c},
     interrupt::{InterruptHandler, Priority, software::SoftwareInterruptControl},
+    otg_fs::Usb,
     rtc_cntl,
     timer::timg::TimerGroup,
 };
@@ -39,40 +40,41 @@ extern "C" fn rotary_handler() {
     rotary::update_encoder();
 }
 
-#[allow(unused)]
-const ROTARY_HANDLER: InterruptHandler = InterruptHandler::new(rotary_handler, Priority::min());
-
-// #[allow(
-//     clippy::large_stack_frames,
-//     reason = "it's not unusual to allocate larger buffers etc. in main"
-// )]
 #[esp_rtos::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: embassy_executor::Spawner) -> ! {
     esp_alloc::heap_allocator!(size: 3 * 32 * 1024);
 
     // Create peripherals and configure CPU clock.
     let config = MCUConfig::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    // Setup timers and software interrupts.
+    // Setup RTOS.
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let sw_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
 
-    // Setting up interrupt GPIO pins.
+    // Setup rotary encoder.
     let dt_pin = peripherals.GPIO36.degrade();
     let clk_pin = peripherals.GPIO40.degrade();
     rotary::init_rotary(dt_pin, clk_pin);
 
     // Real Time Clock
     let rtc = rtc_cntl::Rtc::new(peripherals.LPWR);
-    let delay = esp_hal::delay::Delay::new();
+
+    let usb = Usb::new(peripherals.USB0, peripherals.GPIO20, peripherals.GPIO19);
+    match usb_task(usb) {
+        Ok(task) => spawner.spawn(task),
+        Err(e) => {
+            panic!("Failed to spawn usb task: {:?}", e);
+        }
+    };
     info!("Embassy initialized!");
 
     // Enable GPIO interrupts.
     {
         use esp_hal::{interrupt, peripherals as phrs};
-        interrupt::bind_handler(phrs::Interrupt::GPIO, ROTARY_HANDLER);
+        let rotary_handler = InterruptHandler::new(rotary_handler, Priority::min());
+        interrupt::bind_handler(phrs::Interrupt::GPIO, rotary_handler);
         // interrupt::enable(phrs::Interrupt::GPIO, Priority::min());
     }
 
@@ -113,4 +115,75 @@ async fn main(_spawner: Spawner) -> ! {
 
         embassy_time::Timer::after_millis(20).await;
     }
+}
+
+#[embassy_executor::task]
+async fn usb_task(usb: Usb<'static>) {
+    use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+    use esp_hal::otg_fs::asynch::{Config as OtgConfig, Driver};
+    use static_cell::StaticCell;
+
+    static EP_OUT_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONFIG_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static BOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+    static STATE: StaticCell<State> = StaticCell::new();
+
+    // use alloc::string::String;
+    // static BASE_MAC: StaticCell<String> = StaticCell::new();
+
+    // use esp_hal::efuse::base_mac_address;
+    // let mac_str = base_mac_address()
+    //     .as_bytes()
+    //     .iter()
+    //     .fold(String::new(), |acc, byte| format!("{acc}{:X}", byte));
+
+    let mut config = embassy_usb::Config::new(0x1209, 0x0001);
+    config.manufacturer = Some("Volumize");
+    config.product = Some("Volumize USB");
+    // config.serial_number = Some(BASE_MAC.init(mac_str));
+    config.serial_number = Some("VMZE:01");
+
+    let mut builder = embassy_usb::Builder::new(
+        Driver::new(usb, EP_OUT_BUFFER.init([0u8; 256]), OtgConfig::default()),
+        config,
+        CONFIG_DESC.init([0u8; 256]),
+        BOS_DESC.init([0u8; 256]),
+        &mut [],
+        CONTROL_BUF.init([0u8; 64]),
+    );
+
+    let state = STATE.init(State::new());
+    let mut class = CdcAcmClass::new(&mut builder, state, 64);
+
+    let mut usb_device = builder.build();
+    let mut ticker = Ticker::every(Duration::from_millis(1));
+
+    let usb_future = usb_device.run();
+
+    let echo_future = async {
+        loop {
+            class.wait_connection().await;
+            ticker.next().await;
+            info!("USB connected");
+
+            let mut buf = [0u8; 64];
+            loop {
+                match class.read_packet(&mut buf).await {
+                    Ok(count) => {
+                        info!("read {} bytes", count);
+                        if count > 0 {
+                            let data = &buf[..count];
+                            info!("data: {}", data);
+                        }
+                    }
+                    Err(error) => {
+                        info!("read error: {}", error);
+                    }
+                }
+            }
+        }
+    };
+
+    embassy_futures::join::join(usb_future, echo_future).await;
 }
